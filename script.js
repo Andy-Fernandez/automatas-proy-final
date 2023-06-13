46cd4f5156edbfed5326391df5b5093e4e4a4b93
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2023-06-13 11:59:07 -0400

    Cifrado de transpocición funcionando correctamente con decifrado incluido, datos personales e introducción

--- script.js
+++ script.js
@@ -23,3 +23,38 @@ document.getElementById('encryptButton').addEventListener('click', function() {
 
   document.getElementById('outputText').value = outputText;
 });
+
+document.getElementById('decryptButton').addEventListener('click', function() {
+    const inputText = document.getElementById('inputText').value;
+    const columnOrder = document.getElementById('columnOrder').value.split(',').map(Number);
+    const numColumns = columnOrder.length;
+    let outputText = '';
+
+    // Crear una matriz para almacenar las columnas
+    let columns = [];
+    for (let i = 0; i < numColumns; i++) {
+        columns.push([]);
+    }
+
+    // Llenar las columnas con los caracteres del mensaje cifrado
+    let j = 0;
+    for (let i = 0; i < numColumns; i++) {
+        let columnIndex = columnOrder[i] - 1;
+        let columnLength = Math.ceil((inputText.length - j) / (numColumns - i));
+        columns[columnIndex] = inputText.slice(j, j + columnLength).split('');
+        j += columnLength;
+    }
+
+    // Leer las columnas en el orden original y concatenar los caracteres
+    let k = 0;
+    while (k < inputText.length) {
+        for (let i = 0; i < numColumns; i++) {
+            if (columns[i].length > 0) {
+                outputText += columns[i].shift();
+                k++;
+            }
+        }
+    }
+
+    document.getElementById('outputText').value = outputText;
+});
